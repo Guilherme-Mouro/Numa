b9861a6b5855809615bee9bb3959f4fd7856c9a6
Guilherme-Mouro <guilhermesimoesmouro@gmail.com>
2025-11-29 17:16:30 +0000

    Fix database bug

--- app/src/main/java/com/example/numa/DataBase.kt
+++ app/src/main/java/com/example/numa/DataBase.kt
@@ -17,7 +17,7 @@ import com.example.numa.entity.AchievementUser
 
 @Database(
     entities = [User::class, Pet::class, Habit::class, Sleep::class, Achievement::class, AchievementUser::class],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 
